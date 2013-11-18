aa9eeeac2709795c0e6ade82aab66891f86a936c
Istvan Laszlovszky <istvan.laszlovszky@gmail.com>
2013-11-18 17:20:03 +0100

    password protected app

---
+++ rails_projects/password_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecretApp::Application.config.secret_key_base = '67ab04a46e1cb826a46c130fdedadb6ff9acd01f2d0f64a6d922b65116db29b107ca508be03dbd3812aeaa97f56c171ea37669d858160f921360f7bbf3f4e6b2'
---
+++ rails_projects/password_app/test/helpers/secret_page_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SecretPageHelperTest < ActionView::TestCase
+end
