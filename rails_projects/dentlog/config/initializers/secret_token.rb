50568766017ef69d7f7e24d6f8a692c86f682329
Istvan Laszlovszky <istvan.laszlovszky@gmail.com>
2013-11-20 22:56:25 +0100

    Add a StaticPages controller

---
+++ rails_projects/dentlog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dentlog::Application.config.secret_key_base = '3209bb9084582646736ac82a4d7e710d5ebaf58c1c2364f66f9cde44eab479289981c4d5396244e75b8821d89900f74ad7bb61e6765303dc63d71f723fd0b464'
